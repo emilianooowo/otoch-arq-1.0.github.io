6565a6d05fa27583589cb4bcf037a721b2f63fbb
emilianoowo <e82040117@gmail.com>
2025-11-27 12:26:56 -0600

    index done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerBtn = document.querySelector('.hamburger-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    hamburgerBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('open');
+    });
+
+    const mobileLinks = mobileMenu.querySelectorAll('a');
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            mobileMenu.classList.remove('open');
+            hamburgerBtn.classList.remove('is-active'); 
+        });
+    });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
     const slide = document.querySelector(".slide");
 
